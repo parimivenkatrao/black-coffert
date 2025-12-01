58a1c6b0e366948098f8ea0410d1f5521698d8c5
PARIMI VENKATARAO <parimivenkatrao7@gmail.com>
2025-12-01 18:40:38 +0530

    reslove issues

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/black-coffert /"
   plugins: [react()],
-  server: {
-    port: 5173
-  }
+  //server: {
+  //   port: 5173
+  //}
 });
